54ee59e8baefead4cb855f04a7246249a7dfd8d2
lenovo <lenovo@ubuntu.(none)>
2012-08-11 00:31:46 -0400

    test updated

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -10,4 +10,11 @@ describe Factorial::Base do
     fact = Factorial::Base.new(0)
     fact.factorial_result.first.should == 1
   end
+
+  it "checking the factorial of multiple numbers" do
+    fact = Factorial::Base.new(5,0,8)
+    fact.factorial_result[0].should == 120
+    fact.factorial_result[1].should == 1
+    fact.factorial_result[2].should == 40320
+  end
 end
