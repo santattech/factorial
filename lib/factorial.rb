c6659e2c08f420f08be2e6cee965973980f81e0a
lenovo <lenovo@ubuntu.(none)>
2012-08-15 23:48:08 -0400

    factorial.rb changed

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,11 +1,19 @@
 require 'factorial/result'
+require 'factorial/gcf'
+
 module Factorial
 
   class Base
-    attr_reader :factorial_result
+    attr_reader :factorial_result, :gcf_result
 
     def initialize(*args)
       @factorial_result = Factorial::Result.new(*args)
+      @gcf_result = Factorial::Gcf.new(*args)
     end
+
+    def greatest_common_factor
+      @gcf = Factorial::Gcf.new(*args)
+    end
+
   end
 end
